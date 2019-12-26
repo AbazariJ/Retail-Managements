e9bbbc15dbd467cfefe113c0d831c86cdc3f45a0
javad abazari <j.abazari71@gmail.com>
2019-12-27 02:04:52 +0330

    little change

--- RMDataManagement/Controllers/ValuesController.cs
+++ RMDataManagement/Controllers/ValuesController.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNet.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
-namespace RMDataManagement.Controllers
+namespace TRMdataManager.Controllers
 {
     [Authorize]
     public class ValuesController : ApiController
